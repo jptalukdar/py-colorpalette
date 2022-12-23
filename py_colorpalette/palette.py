719005fe35cfcb97cf91ffffdd75ddd66f14d204
Jyotiplaban Talukdar <jyotiplaban@gmail.com>
2022-12-23 11:21:42 +0530

    Fixed codestyle

--- py_colorpalette/palette.py
+++ py_colorpalette/palette.py
@@ -1,27 +1,31 @@
 from typing import Any
 
 try:
-  from py_colorpalette import color
+    from py_colorpalette import color
 except ModuleNotFoundError:
-  import color
+    import color
 
-class Palette():
-  def __init__(self, name:str) -> None:
-    self.palette_name = name
-    self.named_palette = {}
-    self.colors = []
-  def __getitem__(self, __name: str) -> color.Color:
-    return self.named_palette[__name]
-  
-  def __setitem__(self, name: str, value: Any) -> None:
-    print(name,value)
-    if not isinstance(value, color.Color):
-      try:
-        value = color.Color(value)
-      except color.InvalidColorCodeException:
-        raise TypeError(f"The item {value} [{type(value)}] is not a color. Use Color().") 
-    self.named_palette[name] = value
-  
+
+class Palette:
+    def __init__(self, name: str) -> None:
+        self.palette_name = name
+        self.named_palette = {}
+        self.colors = []
+
+    def __getitem__(self, __name: str) -> color.Color:
+        return self.named_palette[__name]
+
+    def __setitem__(self, name: str, value: Any) -> None:
+        print(name, value)
+        if not isinstance(value, color.Color):
+            try:
+                value = color.Color(value)
+            except color.InvalidColorCodeException:
+                raise TypeError(
+                    f"The item {value} [{type(value)}] is not a color. Use Color()."
+                )
+        self.named_palette[name] = value
+
 
 # if __name__ == "__main__":
 #   name = "primary"
--- tests/test_palette/test_palette.py
+++ tests/test_palette/test_palette.py
@@ -6,14 +6,14 @@ from py_colorpalette import color, palette
 @pytest.mark.parametrize(
     ("name", "color_code"),
     [
-        ( "primary" , color.Color("850000")),
-        ( "secondary" , "#808080"),
+        ("primary", color.Color("850000")),
+        ("secondary", "#808080"),
     ],
 )
 def test_get_set(name, color_code):
-  """Example test with parametrization."""
-  p = palette.Palette("Triad")
-  p[name] = color_code
-  old = p[name]
-  print("OLD",old, color_code)
-  assert str(old) == str(color_code)
+    """Example test with parametrization."""
+    p = palette.Palette("Triad")
+    p[name] = color_code
+    old = p[name]
+    print("OLD", old, color_code)
+    assert str(old) == str(color_code)
